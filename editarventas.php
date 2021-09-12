a4d0c550c535ffbb2e9e6ee07db230072dba046f
Juan31452 <jcs1011@gmail.com>
2021-09-11 19:32:26 -0500

    se agrega editar ventas

---
+++ editarventas.php
@@ -0,0 +1,33 @@
+<?php
+include_once 'conexion2.php';
+include_once 'calculo.php';
+/*echo 'editar.php?Descripcion=x';
+$Descripcion = $_GET['Descripcion'];
+echo $Descripcion;*/
+
+$Idventa = $_GET['Idventa'];
+$Fecha= $_GET['Fecha'];
+$Producto= $_GET['Producto'];
+$Cantidad= $_GET['Cantidad'];
+$Valor_Unitario= $_GET['Valor_Unitario'];
+$Valor_Total=  multiplica($Cantidad,$Valor_Unitario);
+echo $Idproducto;
+echo '<br>';
+echo $Descripcion;
+
+
+
+$sql_editar = 'UPDATE productos SET Fecha=?,Descripcion=?,Cantidad=?,Valor_unitario=?,Valor_total=? WHERE Idproducto=?';
+ $sentencia_editar = $pdo->prepare($sql_editar);
+ 
+ $sentencia_editar->execute(array(
+  $Fecha,$Descripcion,$Cantidad,
+  $Valor_Unitario,$Valor_Total,$Idproducto
+ )); 
+
+ //cerrar conexion
+$pdo=null;
+$sentencia_eliminar = null;
+
+ header('location:index.php');
+?>
