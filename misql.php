87ae2e51588e3bbeb688fe7c8d98d2a138f0e5e0
Juan31452 <jcs1011@gmail.com>
2022-05-11 20:08:37 -0500

    se crean consultas sql

--- misql.php
+++ misql.php
@@ -1,6 +1,5 @@
 <?php
 require_once('Clases/fecha.php');
-//include_once('conexion2.php');
 include_once('Clases/ConexionPDO.php');
 
 function consultar($tabla)
@@ -12,8 +11,16 @@ function consultar($tabla)
     echo $objfecha->año_actual;
 
     $miconexion1 = new conexion;
-    $consulta = $miconexion1->prepare('SELECT * FROM '.$tabla.' WHERE MONTH(Fecha) = 04 AND YEAR(Fecha) = 2022 ');
-    $consulta->execute();
+    $consulta = $miconexion1->prepare('SELECT * FROM '.$tabla.' WHERE MONTH(Fecha) = ? AND YEAR(Fecha) = ? ');
+    $consulta->execute(array($objfecha->mes_actual,$objfecha->año_actual));
+    $registro = $consulta->fetch();
+    echo "<pre>";
+    var_dump($registro);
+    echo "</pre>";
+
+    $miconexion2 = new conexion;
+    $consulta = $miconexion2->prepare('SELECT SUM(Valor_Total) FROM '.$tabla.' WHERE MONTH(Fecha) = ? AND YEAR(Fecha) = ? ');
+    $consulta->execute(array($objfecha->mes_actual,$objfecha->año_actual));
     $registro = $consulta->fetch();
     var_dump($registro);
 
--- principal.php
+++ principal.php
@@ -1,8 +1,5 @@
 <?php
-//include_once('conexion2.php');
-include_once('Clases/ConexionPDO.php');
 include_once('calculo.php');
-require_once('Clases/fecha.php');
 include_once('misql.php');
 
 $tabla = "Gastos";
@@ -11,3 +8,38 @@ $mensaje = consultar($tabla = "Gastos");
 echo $mensaje;
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+ <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>REGISTRO</title>
+ </head>
+    <body>
+    <div class="contenedor" >
+            <form method="GET" >
+                <select name="tabla" id="tabla">
+                    <option>Gastos</option>
+                    <option>Produccion</option>
+                    <option>Ventas</option>
+                </select>
+                <select name="mes" id="mes">
+                    <option>01</option>
+                    <option>02</option>
+                    <option>03</option>
+                    <option>04</option>
+                    <option>05</option>
+                    <option>06</option>
+                </select>
+                <select name="año" id="año">
+                    <option>2022</option>
+                    <option>2021</option>  
+                </select>
+                <input class="button" type="submit" name ="enviar" value="BUSCAR" />
+            </form>
+        </div> 
+
+    </body>
+</html>
