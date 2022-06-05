7cf7f17ddef8cf45797691a3f9c8b74df11805e2
Juan31452 <jcs1011@gmail.com>
2022-06-05 00:13:36 -0500

    se agrega apiventas

---
+++ Clases/APIventas.php
@@ -0,0 +1,35 @@
+<?php
+
+include_once 'Clases/Claseventas.php';
+
+class APIventas
+{
+    function getAll(){
+        $miventa = new Claseventas();
+        $venta = array();
+        $venta = array();
+
+        $res = $miventa->consulta();
+
+        if($res->rowCount()){
+            while ($row = $res->fetch(PDO::FETCH_ASSOC)){
+    
+                $item=array(
+                    "Idventa" => $row['Idventa'],
+                    "Fecha" => $row['Fecha'],
+                    "Producto" => $row['Producto'],
+                    "Cantidad" => $row['Cantidad'],
+                    "Valor_Unitario" => $row['Valor_Unitario']
+
+                );
+                array_push($venta,$item);
+            }
+        
+            echo json_encode($venta);
+        }else{
+            echo json_encode(array('mensaje' => 'No hay elementos'));
+        }
+    }
+}
+
+?>
